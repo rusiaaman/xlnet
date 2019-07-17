dc67523630be4ce263ad1bf17d95fd3399302cef
Aman Rusia <aman@suprath.com>
2019-07-17 11:31:47 +0530

    Fixed assertion

--- modeling.py
+++ modeling.py
@@ -233,9 +233,11 @@ def relative_positional_encoding(qlen, klen, d_model, clamp_len, attn_type,
 
     if bsz is not None:
       # With bi_data, the batch size should be divisible by 2.
-      assert bsz%2 == 0
-      fwd_pos_emb = positional_embedding(fwd_pos_seq, inv_freq, bsz//2)
-      bwd_pos_emb = positional_embedding(bwd_pos_seq, inv_freq, bsz//2)
+      assert_op = tf.Assert(tf.equal(bsz%2,0), ['Batch size should'
+                  ' be divisble by two for bi_data. Got: ',bsz])
+      with tf.control_dependencies([assert_op]):
+        fwd_pos_emb = positional_embedding(fwd_pos_seq, inv_freq, bsz//2)
+        bwd_pos_emb = positional_embedding(bwd_pos_seq, inv_freq, bsz//2)
     else:
       fwd_pos_emb = positional_embedding(fwd_pos_seq, inv_freq)
       bwd_pos_emb = positional_embedding(bwd_pos_seq, inv_freq)
